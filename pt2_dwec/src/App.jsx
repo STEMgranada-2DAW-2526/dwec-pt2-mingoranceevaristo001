1426f9f134f851d7f125858f1ed5c3c79cea2f7d
evaristomingorance001 <evaristomingorance001.alu@stemgranada.com>
2025-11-25 10:52:46 +0100

    WIP action

--- pt2_dwec/src/App.jsx
+++ pt2_dwec/src/App.jsx
@@ -1,9 +1,35 @@
 import { useState } from 'react'
-
 import './App.css'
+import cookieImg from "./assets/galleta.png";
+import cursorImg from "./assets/cursor.png";
+import grandmaImg from "./assets/abuela.png";
+import multiplierImg from "./assets/corre.png";
+
+const initialState = {
+  damageDealt: 0,
+
+  waveGoal: 100,
+  caramels: 20,
+  damagePerShot: 1,
+
+  autoShotsPerSecond: 1,
+  upgrades: []
+};
 
 function App() {
   const [count, setCount] = useState(0)
+
+  function caramelReducer(state, action){
+    let outputState = state;
+    
+     if (action.type == 'CLICK_CANION') {
+      outputState = { ...state, damageDealt: state.damageDealt + state.damagePerShot }
+    }
+
+    return outputState;
+  }
+
+  const [state, dispatch] = useReducer(caramelReducer, INITIAL_STATE)
 
   return (
     <>
